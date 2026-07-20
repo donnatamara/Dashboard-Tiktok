b48c18910bf9e9622689e6426bdd3ce9f0106560
Donna Nur Tamara <donnatamara820@gmail.com>
2026-07-20 10:02:58 +0700

    chore: remove unused files and dead code
    Deleted: SummaryCards.tsx (never imported), assets (hero.png, react.svg, vite.svg - boilerplate leftovers), public (favicon.svg, icons.svg - unreferenced), _check_data.py (one-off dev script). Also removed duplicate TikTokIcon default export from Icons.tsx (already has dedicated TikTokIcon.tsx).

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -131,10 +131,3 @@ export function FacebookIcon({ size = 20, color = 'currentColor' }: Props) {
   );
 }
 
-export default function TikTokIcon({ size = 36, color = 'white' }: Props) {
-  return (
-    <svg width={size} height={size} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <path d="M72.8 19.2c-3.5-4.1-5.6-9.3-5.8-14.8h-3.2l-.1.1V43c0 6.6-5.4 12-12 12s-12-5.4-12-12 5.4-12 12-12c1.2 0 2.3.2 3.4.5v-3.3c-1.1-.2-2.3-.3-3.4-.3-8.6 0-15.5 6.9-15.5 15.5s6.9 15.5 15.5 15.5c7.7 0 14.1-5.6 15.3-12.9l.2-38.2c.1 0 .1 0 .2.1 2.3.6 4.5 1.6 6.4 3.1 0 0 0 0 .1.1 2.3 1.7 4.3 3.9 5.7 6.5h.1z" fill={color} fill-rule="evenodd"/>
-    </svg>
-  );
-}
